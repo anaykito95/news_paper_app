a10e199d97adfb28da33ba2913f1ab06a5c5eebc
Andrés Forns Jusino <andresito.jusino@gmail.com>
2020-08-17 14:52:01 -0400

    Adding developer info on home screen

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_paper/ui/app_theme.dart';
 import 'package:news_paper/ui/page/newspaper_list.dart';
+import 'package:package_info/package_info.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
@@ -12,30 +13,54 @@ class Home extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text(
-            'Periodicos de Cuba',
-            style: Theme.of(context).textTheme.headline5.copyWith(color: Colors.black87),
+          title: ListTile(
+            title: Text(
+              'Periódicos de Cuba',
+              style: Theme.of(context).textTheme.headline5.copyWith(color: Colors.black87),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.info_outline),
+              onPressed: () => PackageInfo.fromPlatform().then((info) => showAboutDialog(
+                      context: context,
+                      applicationIcon: CircleAvatar(
+                        backgroundImage: AssetImage('assets/icon/icon.png'),
+                      ),
+                      children: [
+                        ListTile(
+                          title: Text('Desarrollador'),
+                          subtitle: Text('Ing. Andrés Forns Jusino'),
+                        ),
+                        ListTile(
+                          title: Text('Version'),
+                          subtitle: Text('${info.version}.${info.buildNumber}'),
+                        )
+                      ])),
+            ),
           ),
           backgroundColor: Colors.white,
           iconTheme: IconThemeData(color: Colors.white),
         ),
 //        drawer: Drawer(),
-        bottomNavigationBar: BottomAppBar(
-//        color: Colors.black26,
-          elevation: 0,
-          child: ListTile(
-            subtitle: Text(
-              'Ing. Andrés Forns Jusino',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-            title: Text(
-              'Desarrollador',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-          ),
-        ),
+//        bottomNavigationBar: BottomAppBar(
+////        color: Colors.black26,
+//          elevation: 0,
+//          child: ListTile(
+//            subtitle: Text(
+//              'Ing. Andrés Forns Jusino',
+////              textAlign: TextAlign.center,
+//              style: Theme.of(context).textTheme.bodyText1,
+//            ),
+//            title: Text(
+//              '*Desarrollador',
+////              textAlign: TextAlign.center,
+//              style: Theme.of(context).textTheme.bodyText2,
+//            ),
+//            trailing: Text(
+//              'asd',
+//              style: Theme.of(context).textTheme.caption.copyWith(color: Colors.black87),
+//            ),
+//          ),
+//        ),
         body: Column(
           children: <Widget>[
             Expanded(child: Container(child: NewsPaperList())),
